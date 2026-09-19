699dbe061dc152450d6bdf4ec28cc1f9ea11a65a
7lpzk <kaualpferreira@gmail.com>
2026-09-19 16:30:01 -0300

    Conectar agendamento ao Apps Script publicado

--- js/config.js
+++ js/config.js
@@ -1,3 +1,2 @@
-// Cole aqui a URL /exec da implantação do Apps Script (veja ATIVAR.md).
-// Esta URL é pública. Não coloque senhas ou chaves secretas neste arquivo.
-window.MAMAE_SERENA_API = '';
+// Endpoint público da agenda. Não incluir senhas neste arquivo.
+window.MAMAE_SERENA_API = 'https://script.google.com/macros/s/AKfycbxF5vq3wH7zJJdEHYC34qPlkfgBMbExI0oLytVdK6kmnb4T2Ic8ReNbzeGTTpafxJUx8g/exec';
